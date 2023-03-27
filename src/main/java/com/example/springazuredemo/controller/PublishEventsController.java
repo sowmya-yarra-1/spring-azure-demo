145a4eabcafab9508c41d08665e3bb20a3ad66ad
Yarra Sowmya <yarra.sowmya@ibm.com>
2023-03-27 16:19:26 +0530

    first commit

---
+++ src/main/java/com/example/springazuredemo/controller/PublishEventsController.java
@@ -0,0 +1,13 @@
+package com.example.springazuredemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PublishEventsController {
+  @RequestMapping("/publishEvent")
+  public String greeting() {
+    return "Event published successfully";
+  }
+
+}
