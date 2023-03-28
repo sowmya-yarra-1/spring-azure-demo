f3d64badd3ac1210434e5d30316d0cc1aa751a48
Yarra Sowmya <yarra.sowmya@ibm.com>
2023-03-28 11:36:48 +0530

    second commit

--- src/main/java/com/example/springazuredemo/controller/PublishEventsController.java
+++ src/main/java/com/example/springazuredemo/controller/PublishEventsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PublishEventsController {
   @RequestMapping("/publishEvent")
   public String greeting() {
-    return "Event published successfully";
+    return "Application deployed successfully on Azure";
   }
 
 }
